e1f80481d63e60eba75562004b52d701a17b835a
billycropper <williamcropper@Billys-MacBook-Pro.local>
2019-03-13 14:17:32 -0400

    added method to Vehicle class

---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,16 @@
+public class Vehicle {
+    public int doors;
+    public int wheels;
+
+
+    public Vehicle(int doors, int wheels){
+        this.doors = doors;
+        this.wheels = wheels;
+    }
+
+
+
+    public String move(){
+        return "move... get out of the way";
+    }
+}
